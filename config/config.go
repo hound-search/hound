3a9ae9e3c9016399a43bb52c571c032808a950e9
Iori Yoneji <yoneji_i@worksap.co.jp>
2018-11-19 16:58:23 +0900

    add example configuration for custom ref repository

--- config/config.go
+++ config/config.go
@@ -29,7 +29,7 @@ type Repo struct {
 	Vcs               string         `json:"vcs"`
 	VcsConfigMessage  *SecretMessage `json:"vcs-config"`
 	UrlPattern        *UrlPattern    `json:"url-pattern"`
-	Ref               string         `json:"vcs-refs"`
+	Ref               string         `json:"ref"`
 	ExcludeDotFiles   bool           `json:"exclude-dot-files"`
 	EnablePollUpdates *bool          `json:"enable-poll-updates"`
 	EnablePushUpdates *bool          `json:"enable-push-updates"`
