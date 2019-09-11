7f65ffdddd2ca263fbeeb17a80db22e6cbd07966
Ivan Pereira <ivan.pereira@booking.com>
2019-09-11 15:00:26 +0200

    Set the jsx extension to all components that return JSX code

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        'ui/js/hound.js': './ui/assets/js/hound.js',
-        'ui/js/excluded_files.js': './ui/assets/js/excluded_files.js',
+        'ui/js/hound.js': './ui/assets/js/hound.jsx',
+        'ui/js/excluded_files.js': './ui/assets/js/excluded_files.jsx',
     },
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
@@ -19,5 +19,8 @@ module.exports = {
     output: {
         filename: '[name]',
         path: path.resolve(__dirname, '.build')
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
     }
 };
