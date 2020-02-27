561a53c39d914859a49b483cefa3ab3ffc39ec4a
Andreas Linz <anli@spreadshirt.net>
2020-02-27 11:13:10 +0100

    Propagate errors from test functions
    Tests will fail if the unmmap error is returned instead of being logged
    (FIXME annotation).

--- codesearch/index/write_test.go
+++ codesearch/index/write_test.go
@@ -115,7 +115,7 @@ func fileList(list ...uint32) string {
 	return string(buf)
 }
 
-func buildFlushIndex(out string, paths []string, doFlush bool, fileData map[string]string) {
+func buildFlushIndex(out string, paths []string, doFlush bool, fileData map[string]string) error {
 	ix := Create(out)
 	ix.AddPaths(paths)
 	var files []string
@@ -129,18 +129,21 @@ func buildFlushIndex(out string, paths []string, doFlush bool, fileData map[stri
 	if doFlush {
 		ix.flushPost()
 	}
-	ix.Flush()
+	return ix.Flush()
 }
 
-func buildIndex(name string, paths []string, fileData map[string]string) {
-	buildFlushIndex(name, paths, false, fileData)
+func buildIndex(name string, paths []string, fileData map[string]string) error {
+	return buildFlushIndex(name, paths, false, fileData)
 }
 
 func testTrivialWrite(t *testing.T, doFlush bool) {
 	f, _ := ioutil.TempFile("", "index-test")
 	defer os.Remove(f.Name())
 	out := f.Name()
-	buildFlushIndex(out, nil, doFlush, trivialFiles)
+	err := buildFlushIndex(out, nil, doFlush, trivialFiles)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	data, err := ioutil.ReadFile(out)
 	if err != nil {
