2f344e9b8b9b46eaf754bc001e653a7bb85cea7c
Tamás Gulácsi <tgulacsi78@gmail.com>
2020-07-09 18:47:41 +0200

    fake: Do not copy, use a symlink

--- vcs/fake.go
+++ vcs/fake.go
@@ -3,11 +3,8 @@ package vcs
 import (
 	"crypto/sha1"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 )
 
@@ -26,7 +23,7 @@ func (g *FakeDriver) HeadRev(dir string) (string, error) {
 	cmd := exec.Command(
 		"tar",
 		"cf", "-",
-		".",
+		"./",
 	)
 	cmd.Dir = dir
 	cmd.Stdout = hsh
@@ -40,50 +37,17 @@ func (g *FakeDriver) HeadRev(dir string) (string, error) {
 const fakeSpec = ".source"
 
 func (g *FakeDriver) Pull(dir string) (string, error) {
-	if dir == "" || dir == "/" {
-		return "", fmt.Errorf("empty dest dir %q", dir)
-	}
-	b, err := ioutil.ReadFile(filepath.Join(dir, fakeSpec))
-	if err != nil {
-		return "", err
-	}
-	src := string(b)
-	if src == "" || src == "/" {
-		return "", fmt.Errorf("empty source dir %q", src)
-	}
-	if err := os.MkdirAll(dir, 0750); err != nil {
-		log.Printf("MkdirAll(%q): %+v", dir, err)
-	}
-	//log.Println("dir:", dir, "src:", src)
-	if err := run("rsync fetch", dir,
-		"rsync",
-		"-a",
-		"--delete-after",
-		src+"/",
-		"./",
-	); err != nil {
-		return "", err
-	}
-	if err := g.writeSource(dir, src); err != nil {
-		return "", err
-	}
-
 	return g.HeadRev(dir)
-}
-
-func (g *FakeDriver) writeSource(dir, src string) error {
-	return ioutil.WriteFile(filepath.Join(dir, fakeSpec), []byte(src), 0640)
 }
 
 func (g *FakeDriver) Clone(dir, url string) (string, error) {
 	src := strings.TrimPrefix(url, "file://")
-	os.MkdirAll(dir, 0750)
-	if err := g.writeSource(dir, src); err != nil {
+	if err := os.Symlink(src, dir); err != nil {
 		return "", err
 	}
 	return g.Pull(dir)
 }
 
 func (g *FakeDriver) SpecialFiles() []string {
-	return []string{fakeSpec}
+	return nil
 }
