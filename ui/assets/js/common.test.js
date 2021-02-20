8c44588a04f365cc37505b6e083e2fc47e18ec85
Alexander Chiu <alexanderchiu@outlook.com>
2021-02-20 18:15:36 +0200

    Add UrlToRepo tests
    See also: GH-259

--- ui/assets/js/common.test.js
+++ ui/assets/js/common.test.js
@@ -19,3 +19,73 @@ describe("ExpandVars", () => {
         expect(ExpandVars(template, {})).toBe(template);
     });
 });
+
+describe("UrlToRepo", () => {
+    test("Generate url from repo with default values", () => {
+        const repo = {
+            url: "https://www.github.com/YourOrganization/RepoOne.git",
+            "url-pattern":
+            {
+                "base-url": "{url}/blob/{rev}/{path}{anchor}",
+                anchor: "#L{line}"
+            }
+        };
+        const path = "test.txt"
+        const line = null
+        const rev = "master"
+        expect(UrlToRepo(repo, path, line, rev)).toBe(
+            "https://www.github.com/YourOrganization/RepoOne/blob/master/test.txt"
+        );
+    });
+
+    test("Generate url from repo with default values and line", () => {
+        const repo = {
+            url: "https://www.github.com/YourOrganization/RepoOne.git",
+            "url-pattern":
+            {
+                "base-url": "{url}/blob/{rev}/{path}{anchor}",
+                anchor: "#L{line}"
+            }
+        };
+        const path = "test.txt"
+        const line = "12"
+        const rev = "master"
+        expect(UrlToRepo(repo, path, line, rev)).toBe(
+            "https://www.github.com/YourOrganization/RepoOne/blob/master/test.txt#L12"
+        );
+    });
+
+    test("Generate url from for ssh style repo with default values", () => {
+        const repo = {
+            url: "git@github.com:YourOrganization/RepoOne.git",
+            "url-pattern":
+            {
+                "base-url": "{url}/blob/{rev}/{path}{anchor}",
+                anchor: "#L{line}"
+            }
+        };
+        const path = "test.txt"
+        const line = null
+        const rev = "master"
+        expect(UrlToRepo(repo, path, line, rev)).toBe(
+            "//github.com/YourOrganization/RepoOne/blob/master/test.txt"
+        );
+    });
+
+    test("Generate url from for ssh bitbucket mercurial style repo", () => {
+        const repo = {
+            url: "ssh://hg@bitbucket.org/YourOrganization/RepoOne",
+            "url-pattern":
+            {
+                "base-url" : "{url}/src/master/{path}{anchor}",
+                "anchor" : "#{filename}-{line}"
+            }
+        };
+        const path = "test.txt"
+        const line = null
+        const rev = "master"
+        expect(UrlToRepo(repo, path, line, rev)).toBe(
+            "//bitbucket.org/YourOrganization/RepoOne/src/master/test.txt"
+        );
+    });
+});
