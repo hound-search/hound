2da3beb55d40d95d66f2415f576488b5e4a22e25
Daniel Sullivan <daniel.j.sullivan@state.mn.us>
2023-09-06 10:55:22 -0500

    Improved config reload handling
    * allowing for deleted repositories
    * recreation of event handling fixed
    * mutex for only one config at a time

--- cmds/houndd/main.go
+++ cmds/houndd/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/blang/semver/v4"
+	"github.com/fsnotify/fsnotify"
 	"log"
 	"os"
 	"os/exec"
@@ -11,15 +12,13 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
+	"sync"
 	"syscall"
 
-	"github.com/fsnotify/fsnotify"
 	"github.com/hound-search/hound/config"
 	"github.com/hound-search/hound/searcher"
 	"github.com/hound-search/hound/web"
 )
-
-const gracefulShutdownSignal = syscall.SIGTERM
 
 var (
 	info_log   *log.Logger
@@ -70,10 +69,19 @@ func handleShutdown(shutdownCh <-chan os.Signal, searchers map[string]*searcher.
 	}()
 }
 
-func registerShutdownSignal() <-chan os.Signal {
+func registerShutdownSignal() chan os.Signal {
 	shutdownCh := make(chan os.Signal, 1)
-	signal.Notify(shutdownCh, gracefulShutdownSignal)
+	signal.Notify(shutdownCh, syscall.SIGTERM)
+	signal.Notify(shutdownCh, syscall.SIGINT)
 	return shutdownCh
+}
+
+func unregisterShutdownSignal(shutdownCh chan os.Signal) {
+	if shutdownCh == nil {
+		return
+	}
+	signal.Stop(shutdownCh)
+	shutdownCh = nil
 }
 
 // TODO: Automatically increment this when building a release
@@ -106,13 +114,26 @@ func main() {
 
 	var cfg config.Config
 
-	loadConfig := func() {
+	var shutdownCh chan os.Signal
+	shutdownCh = nil
+	var configUpdateLock sync.Mutex
+
+	loadConfig := func(server *web.Server) {
+		configUpdateLock.Lock()
+		defer configUpdateLock.Unlock()
+		// store existing cfg to check if it's changed
+		cfgJson, _ := cfg.ToJsonString()
+
 		if err := cfg.LoadFromFile(*flagConf); err != nil {
 			panic(err)
 		}
+		// unregister shutdown signal to create a new one
+		unregisterShutdownSignal(shutdownCh)
+
 		// It's not safe to be killed during makeSearchers, so register the
 		// shutdown signal here and defer processing it until we are ready.
-		shutdownCh := registerShutdownSignal()
+		shutdownCh = registerShutdownSignal()
+
 		ok, err := makeSearchers(&cfg, idx)
 		if err != nil {
 			log.Panic(err)
@@ -122,15 +143,20 @@ func main() {
 		} else {
 			info_log.Println("All indexes built!")
 		}
+		// handle shutdown signal now
 		handleShutdown(shutdownCh, idx)
+		if server != nil {
+			// if server has been passed, now check if cfg json has been changed
+			newCfgJson, err := cfg.ToJsonString()
+			if (err == nil) && (cfgJson != newCfgJson) {
+				// cfg json changed, update the server
+				info_log.Println("configJson updated, reloading server")
+				if err := server.UpdateServeWithIndex(idx); err != nil {
+					error_log.Printf("updating server failed for some reason: %s", err)
+				}
+			}
+		}
 	}
-	loadConfig()
-
-	// watch for config file changes
-	configWatcher := config.NewWatcher(*flagConf)
-	configWatcher.OnChange(func(fsnotify.Event) {
-		loadConfig()
-	})
 
 	// Start the web server on a background routine.
 	ws := web.Start(&cfg, *flagAddr, *flagDev)
@@ -139,6 +165,11 @@ func main() {
 	if strings.HasPrefix(host, ":") { //nolint
 		host = "localhost" + host
 	}
+	info_log.Printf("started server without indexes at http://%s\n", host)
+
+	// Initial config load
+	loadConfig(nil)
+	info_log.Printf("loaded config")
 
 	if *flagDev {
 		info_log.Printf("[DEV] starting webpack-dev-server at localhost:8080...")
@@ -151,6 +182,14 @@ func main() {
 			error_log.Println(err)
 		}
 	}
+
+	// watch for config file changes
+	configWatcher := config.NewWatcher(*flagConf)
+	configWatcher.OnChange(
+		func(fsnotify.Event) {
+			loadConfig(ws)
+		},
+	)
 
 	info_log.Printf("running server at http://%s\n", host)
 
